da3ee292d975c39169a14a7480f120555440e229
Vicktoriia <105809872+Vicktoriia@users.noreply.github.com>
2023-04-11 21:15:41 +0000

    Deploying to gh-pages from @ Vicktoriia/grandexllc@42622c76ec7e3627e53ce48b85a0abe03f4e4c0d 🚀

---
+++ index.3d95f322.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.querySelector("[data-header]"),n=e.offsetTop;window.pageYOffset>n?e.classList.add("js-no-transparency"):e.classList.remove("js-no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.3d95f322.js.map
