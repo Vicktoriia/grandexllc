7f475b98260c2e20ad0e21f7e3158fc94bddb779
Vicktoriia <105809872+Vicktoriia@users.noreply.github.com>
2023-04-20 15:55:51 +0000

    Deploying to gh-pages from @ Vicktoriia/grandexllc@5826fe33c873f6ad05f81a174ba1cfce8f2d52d3 🚀

---
+++ index.47f380f4.js
@@ -0,0 +1,2 @@
+function myFunction(){document.getElementById("myDropdown").classList.toggle("show")}window.onclick=function(o){if(!o.target.matches(".dropbtn")){var t=document.getElementById("myDropdown");t.classList.contains("show")&&t.classList.remove("show")}};
+//# sourceMappingURL=index.47f380f4.js.map
